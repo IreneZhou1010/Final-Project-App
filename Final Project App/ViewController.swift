06eb29f827041c86405e38616b7656fe3f2a1b2b
IreneZhou1010 <irenezhou@wustl.edu>
2021-11-06 12:23:19 -0500

    11/6/21 Irene

--- Final Project App/ViewController.swift
+++ Final Project App/ViewController.swift
@@ -8,13 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var rosterButton: UIButton!
+    @IBOutlet weak var announcementsButton: UIButton!
+    @IBOutlet weak var calendarButton: UIButton!
+    @IBOutlet weak var resourcesButton: UIButton!
+    @IBOutlet weak var guruButton: UIButton!
+    @IBOutlet weak var execButton: UIButton!
+    @IBOutlet weak var messagingButton: UIButton!
+    @IBOutlet weak var galleryButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        // Created by Irene, 11/1/21
+        rosterButton.layer.cornerRadius = 10
+        announcementsButton.layer.cornerRadius = 10
+        calendarButton.layer.cornerRadius = 10
+        resourcesButton.layer.cornerRadius = 10
+        guruButton.layer.cornerRadius = 10
+        execButton.layer.cornerRadius = 10
+        messagingButton.layer.cornerRadius = 10
+        galleryButton.layer.cornerRadius = 10
+
+        rosterButton.layer.borderWidth = 2.0
+        announcementsButton.layer.borderWidth = 2.0
+        calendarButton.layer.borderWidth = 2.0
+        resourcesButton.layer.borderWidth = 2.0
+        guruButton.layer.borderWidth = 2.0
+        execButton.layer.borderWidth = 2.0
+        messagingButton.layer.borderWidth = 2.0
+        galleryButton.layer.borderWidth = 2.0
+
+        rosterButton.layer.borderColor = UIColor.black.cgColor
+        announcementsButton.layer.borderColor = UIColor.black.cgColor
+        calendarButton.layer.borderColor = UIColor.black.cgColor
+        resourcesButton.layer.borderColor = UIColor.black.cgColor
+        guruButton.layer.borderColor = UIColor.black.cgColor
+        execButton.layer.borderColor = UIColor.black.cgColor
+        messagingButton.layer.borderColor = UIColor.black.cgColor
+        galleryButton.layer.borderColor = UIColor.black.cgColor
     }
 
-
+    
+    
 }
 
