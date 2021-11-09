cd7ef6a86a27fe94db9dd8598dda52e4f9a33508
Lauren Sands <lauren.sands@wustl.edu>
2021-11-09 16:52:37 -0600

    users can know sign up and login using Firebase

---
+++ Final Project App/Helpers/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  Constants
+//
+//  Created by Lauren Sands on 11/9/21.
+//
+
+import Foundation
---
+++ Final Project App/Helpers/Utilities.swift
@@ -0,0 +1,8 @@
+//
+//  Utilities.swift
+//  Utilities
+//
+//  Created by Lauren Sands on 11/9/21.
+//
+
+import Foundation
