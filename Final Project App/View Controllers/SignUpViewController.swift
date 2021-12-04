c99f7b4b6797c84d32cb535c7a62f5ed34531d0a
gabifowler <gabriellefowler@wustl.edu>
2021-12-04 11:04:38 -0600

    commented out line that wasn't working

--- Final Project App/View Controllers/SignUpViewController.swift
+++ Final Project App/View Controllers/SignUpViewController.swift
@@ -95,6 +95,8 @@ class SignUpViewController: UIViewController {
                             pChanger?.displayName = firstName + " " + lastName
                             pChanger?.photoURL = URL(string: "http://assets.stickpng.com/images/585e4bf3cb11b227491c339a.png")
                             pChanger?.commitChanges()
+                            print("This user is ", Auth.auth().currentUser?.displayName)
+                            print("Their photo url is ", Auth.auth().currentUser?.photoURL)
                             self.transitionToHome()
                         }
                     }
