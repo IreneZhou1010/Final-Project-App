6a4cb8952b3f9e4f7815bfa30848cba43d035f62
Lauren Sands <lauren.sands@wustl.edu>
2021-12-06 03:36:42 -0600

    gabi and lauren added code

--- Final Project App/View Controllers/MessagingViewController.swift
+++ Final Project App/View Controllers/MessagingViewController.swift
@@ -8,30 +8,165 @@
 import UIKit
 import Firebase
 
-class MessagingViewController: UIViewController {
+class MessagingViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
     @IBOutlet weak var profilePic: UIImageView!
     
     @IBOutlet weak var usersName: UILabel!
+    var imagePicker = UIImagePickerController()
+    
+    var theData: [UIImage] = []
+    var photoCount = 0
+    
+    private let storage = Storage.storage().reference()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        fetchData()
+        
         //profilePic.layer.cornerRadius = 100
         
-        print("Photo url is ", Auth.auth().currentUser?.photoURL)
+        print("Current photo url is ", Auth.auth().currentUser?.photoURL)
+        
+        
+        
         let url = (Auth.auth().currentUser?.photoURL)!
         
-        if let data = try? Data(contentsOf: url) {
+        let stringversionofurl = url.absoluteString
+    
+        
+        self.storage.child(stringversionofurl).downloadURL(completion: { url, error in
+            guard let url = url, error == nil else{
+                return
+            }
+            
+            let task = URLSession.shared.dataTask(with: url, completionHandler:  { data, _, error in
+                guard let data = data, error == nil else{
+                    print("Error Occured")
+                    return
+                }
+                DispatchQueue.main.async {
+                    let image = UIImage(data: data)
+                    self.profilePic.image = image
+                }
+               
+            })
+            task.resume()
+            //self.collectionView.reloadData()
+            
+        })
+        
+        /*if let data = try? Data(contentsOf: possiblePath) {
                         if let image = UIImage(data: data) {
                             profilePic.image = image
                         }
-                    }
+                    }*/
         
         usersName.text = Auth.auth().currentUser?.displayName
 
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func changeProfilePicture(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true //crop image to square
+        self.present(imagePicker, animated: true, completion: nil)
+        
+        let url = (Auth.auth().currentUser?.photoURL)!
+        
+        let stringversionofurl = url.absoluteString
+    
+        
+       
+        
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        self.dismiss(animated: true, completion: nil)
+        guard let image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage else{
+            return
+        }
+        guard let imageData = image.pngData() else{
+            return
+        }
+        
+        photoCount = theData.count
+        photoCount = photoCount + 1
+            
+        let photoPath = "ProfilePictures/image" + String(photoCount)
+        let profileChange = Auth.auth().currentUser?.createProfileChangeRequest()
+        profileChange?.photoURL = URL(string: photoPath)
+        profileChange?.commitChanges()
+        
+        storage.child(photoPath).putData(imageData, metadata: nil, completion: { _, error in
+            guard error == nil else{
+                print("Failed to Upload")
+                return
+            }
+            //get download url
+            self.storage.child(photoPath).downloadURL(completion: { url, error in
+                guard let url = url, error == nil else{
+                    return
+                }
+                
+                let task = URLSession.shared.dataTask(with: url, completionHandler:  { data, _, error in
+                    guard let data = data, error == nil else{
+                        print("Error Occured")
+                        return
+                    }
+                    DispatchQueue.main.async {
+                        let image = UIImage(data: data)
+                        self.profilePic.image = image
+                    }
+                   
+                })
+                task.resume()
+                //self.collectionView.reloadData()
+                
+            })
+            
+        })
 
+        
+        
+    }
+    
+    
+    func fetchData(){
+       
+        let photoPath = "ProfilePictures"
+        
+            self.storage.child(photoPath).listAll(completion: {list, error in
+                if let error = error{
+                    return
+                }
+                for item in list.items{
+                    item.downloadURL(completion: { url, error in
+                        guard let url = url, error == nil else{
+                            return
+                        }
+                        
+                        let task = URLSession.shared.dataTask(with: url, completionHandler:  { data, _, error in
+                            guard let data = data, error == nil else{
+                                print("Error Occured")
+                                return
+                            }
+                            DispatchQueue.main.async {
+                                let image = UIImage(data: data)
+                                self.theData.append(image ?? UIImage(named: "noImage")!)
+                                
+                            }
+                           
+                        })
+                        task.resume()
+                    })
+                }
+            })
+            
+    }
+    
     /*
     // MARK: - Navigation
 
@@ -43,3 +178,4 @@ class MessagingViewController: UIViewController {
     */
 
 }
+
--- Final Project App/View Controllers/SinglePictureViewController.swift
+++ Final Project App/View Controllers/SinglePictureViewController.swift
@@ -13,17 +13,32 @@ class SinglePictureViewController: UIViewController {
     @IBOutlet var imageView: UIImageView!
     
     var theImage: UIImage = UIImage(named: "noImage")!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = UIColor.white
-        let imageFrame = CGRect(x: view.frame.midX - 175, y: 200, width: 350, height: 350)
+        var saveButton = UIButton()
         
-        let imageView2 = UIImageView(frame: imageFrame)
-        imageView2.contentMode = .scaleAspectFit
-        imageView2.image = theImage
-        view.addSubview(imageView2)
-    }
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            view.backgroundColor = UIColor.white
+            let imageFrame = CGRect(x: view.frame.midX - 175, y: 200, width: 350, height: 350)
+            
+            let imageView2 = UIImageView(frame: imageFrame)
+            imageView2.contentMode = .scaleAspectFit
+            imageView2.image = theImage
+            view.addSubview(imageView2)
+            
+            let buttonFrame = CGRect(x: view.frame.midX - 50, y: 550, width: 100, height: 100)
+            saveButton = UIButton(frame: buttonFrame)
+            saveButton.setTitle("Save Image", for: .normal)
+            saveButton.setTitleColor(UIColor.black, for: .normal)
+            saveButton.addTarget(self, action: #selector(self.savePhoto), for: .touchUpInside)
+            view.addSubview(saveButton)
+        }
+        
+        @objc func savePhoto(){
+            UIImageWriteToSavedPhotosAlbum(theImage, self, nil, nil)
+            saveButton.isEnabled = false
+            saveButton.setTitle("Saved!", for: .disabled)
+        }
+
     
 
   
